69a078520073425652008301d78b615ef4b55327
ramenfeast <116684033+ramenfeast@users.noreply.github.com>
2022-11-14 11:01:51 -0500

    Update BV Advanced Training.py
    Added Community group based training

--- BV Advanced Training.py
+++ BV Advanced Training.py
@@ -286,6 +286,100 @@ def ethnic_stack_pipe(clfw, clfb, clfa, clfh, color):
                     )
     return(clf)
 
+# %%% Community Group Trained Metrics
+def comm_spec_metrics(classifier, xtrain, ytrain, predi, predii, prediii, prediv, predt, color,  ):
+    plt.figure(0)
+    plot_learning_curve(classifier, xtrain, ytrain, "Classifier Learning Curve")
+
+    plt.figure(1)
+    RocCurveDisplay.from_estimator(classifier, Xi_test, yi_test)
+    plt.figure(2)
+    RocCurveDisplay.from_estimator(classifier, Xii_test, yii_test)
+    plt.figure(3)
+    RocCurveDisplay.from_estimator(classifier, Xiii_test, yiii_test)
+    plt.figure(4)
+    RocCurveDisplay.from_estimator(classifier, Xiv_test, yiv_test)
+    plt.figure(5)
+    RocCurveDisplay.from_estimator(classifier, Xt_test, yt_test)
+
+    plt.figure(6)
+    plot_confusion_matrix(yi_test, predi, color, "Confusion Matrix",7)
+    plot_confusion_matrix(yii_test, predii, color, "Confusion Matrix",8)
+    plot_confusion_matrix(yiii_test, prediii, color, "Confusion Matrix",9)
+    plot_confusion_matrix(yiii_test, prediv, color, "Confusion Matrix",10)
+    plot_confusion_matrix(yiv_test, predt, color, "Confusion Matrix",11)
+
+    #Ethnic and Community group Accuracy
+    ethnic_acc = ethnic_based_acc(predt, yt_test, es_xttest)
+    print(ethnic_acc)
+    comm_acc = comm_group_acc(predt, yt_test, cs_xttest)
+    print(comm_acc)
+
+    #Ethnic and Community Group Breakdown of Missclassified
+    miss_ethnic = missclass_ethnic(predt, yt_test, es_xttest)
+    print(miss_ethnic)
+
+    miss_comm = missclass_comm(predt, yt_test, cs_xttest)
+    print(miss_comm)
+    
+    return(ethnic_acc, comm_acc, miss_ethnic, miss_comm)
+
+
+#%%% Comm Pipeline
+def comm_train_metric_pipe(classifier, xtrain, ytrain, color):
+
+    if classifier == "Logistic Regression": 
+        classify = LogisticRegression()
+    elif classifier == "Random Forest":
+        classify = RandomForestClassifier()
+    elif classifier == "MNB":
+        classify = MultinomialNB()
+        
+    
+    classify.fit(xtrain, ytrain)
+
+    y_pred_clfi = classify.predict(Xi_test)
+    y_pred_clfii = classify.predict(Xii_test)
+    y_pred_clfiii = classify.predict(Xiii_test)
+    y_pred_clfiv = classify.predict(Xiv_test)
+    y_pred_clft = classify.predict(Xt_test)
+
+    clf_ethnic_acc, clf_comm_acc, clf_miss_ethnic, clf_miss_comm = comm_spec_metrics(
+                    classify, xtrain, ytrain, 
+                    y_pred_clfi,
+                    y_pred_clfii,
+                    y_pred_clfiii,
+                    y_pred_clfiv,
+                    y_pred_clft,
+                    color
+                    )
+    return(classify)
+
+#%%% Comm Based Stacking Classifier Pipeline
+def comm_stack_pipe(clfi, clfii, clfiii, clfiv, color):
+    estimators = [('clfi',clfi), ('clfii', clfii), ('clfiii',clfiii), ('clfiv', clfiv)]
+    clf = StackingClassifier(
+        estimators = estimators, 
+        final_estimator = LogisticRegression(), cv= 'prefit')
+    clf.fit(Xt_train,yt_train)
+    
+    y_pred_clfi = clf.predict(Xi_test)
+    y_pred_clfii = clf.predict(Xii_test)
+    y_pred_clfiii = clf.predict(Xiii_test)
+    y_pred_clfiv = clf.predict(Xiv_test)
+    y_pred_clft = clf.predict(Xt_test)
+
+    clf_ethnic_acc, clf_comm_acc, clf_miss_ethnic, clf_miss_comm = ethnic_spec_metrics(
+                    clf, Xt_train, yt_train, 
+                    y_pred_clfi,
+                    y_pred_clfii,
+                    y_pred_clfiii,
+                    y_pred_clfiv,
+                    y_pred_clft,
+                    color
+                    )
+    return(clf)
+
 # %% Import and Clean Data
 # %%% Import Data
 
@@ -424,13 +518,13 @@ yh_test[yh_test >= 7] = 1
 
 # %% Logistic Regression (Ethnic Isolated)
 #%%% Just White
-clflrw = ethnic_train_metric_pipe("Logistic Regression", Xh_train, yh_train, "Blues")
+clflrw = ethnic_train_metric_pipe("Logistic Regression", Xw_train, yw_train, "Blues")
 
 #%%% Just Black
-clflrb = ethnic_train_metric_pipe("Logistic Regression", Xh_train, yh_train, "Reds")
+clflrb = ethnic_train_metric_pipe("Logistic Regression", Xb_train, yb_train, "Reds")
 
 #%%% Just Asian
-clflra = ethnic_train_metric_pipe("Logistic Regression", Xh_train, yh_train, "Greens")
+clflra = ethnic_train_metric_pipe("Logistic Regression", Xa_train, ya_train, "Greens")
 
 #%%% Just Hispanic
 clflrh = ethnic_train_metric_pipe("Logistic Regression", Xh_train, yh_train, "Purples")
@@ -440,13 +534,13 @@ clflrt = ethnic_stack_pipe(clflrh,clflrb,clflra,clflrh, "Oranges")
 
 #%% Random Forest (Ethnic Isolated)
 #%%% Just White
-clfrfw = ethnic_train_metric_pipe("Random Forest", Xh_train, yh_train, "Blues")
+clfrfw = ethnic_train_metric_pipe("Random Forest", Xw_train, yw_train, "Blues")
 
 #%%% Just Black
-clfrfb = ethnic_train_metric_pipe("Random Forest", Xh_train, yh_train, "Reds")
+clfrfb = ethnic_train_metric_pipe("Random Forest", Xb_train, yb_train, "Reds")
 
 #%%% Just Asian
-clfrfa = ethnic_train_metric_pipe("Random Forest", Xh_train, yh_train, "Greens")
+clfrfa = ethnic_train_metric_pipe("Random Forest", Xa_train, ya_train, "Greens")
 
 #%%% Just Hispanic
 clfrfh = ethnic_train_metric_pipe("Random Forest", Xh_train, yh_train, "Purples")
@@ -455,15 +549,181 @@ clfrft = ethnic_stack_pipe(clfrfw,clfrfb,clfrfa,clfrfh, "Oranges")
 
 #%% Multinomial Naive Bayes (Ethnic Isolated)
 #%%% Just White
-clfmnbw = ethnic_train_metric_pipe("MNB", Xh_train, yh_train, "Blues")
+clfmnbw = ethnic_train_metric_pipe("MNB", Xw_train, yw_train, "Blues")
 
 #%%% Just Black
-clfmnbb = ethnic_train_metric_pipe("MNB", Xh_train, yh_train, "Reds")
+clfmnbb = ethnic_train_metric_pipe("MNB", Xb_train, yb_train, "Reds")
 
 #%%% Just Asian
-clfmnba = ethnic_train_metric_pipe("MNB", Xh_train, yh_train, "Greens")
+clfmnba = ethnic_train_metric_pipe("MNB", Xa_train, ya_train, "Greens")
 
 #%%% Just Hispanic
 clfmnbh = ethnic_train_metric_pipe("MNB", Xh_train, yh_train, "Purples")
+#%%% Stack
+clfmnbt = ethnic_stack_pipe(clfmnbw,clfmnbb,clfmnba,clfmnbh, "Oranges")
+# %% Train Test Split and Normalization (Community Group Isolated)
+#%%% Initial X y split
+X_total = df.iloc[:, :-1]
+y_total = df.iloc[:, -1]
+
+# %%% Initial Test Train Split
+Xt_train, Xt_test, yt_train, yt_test = train_test_split(
+    X_total, y_total, test_size=0.2, random_state=1)
+# %%% Sort and Split Ethnicities
+X_I = Xt_train.loc[Xt_train['Community groupc '] == 'I']
+X_II = Xt_train.loc[Xt_train['Community groupc '] == 'II']
+X_III = Xt_train.loc[Xt_train['Community groupc '] == 'III']
+X_IV = Xt_train.loc[Xt_train['Community groupc '] == 'IV']
+
+y_I = yt_train.loc[Xt_train['Community groupc '] == 'I']
+y_II = yt_train.loc[Xt_train['Community groupc '] == 'II']
+y_III = yt_train.loc[Xt_train['Community groupc '] == 'III']
+y_IV = yt_train.loc[Xt_train['Community groupc '] == 'IV']
+
+
+# %%% Test Train Split
+Xi_train, Xi_test, yi_train, yi_test = train_test_split(
+    X_I, y_I, test_size=0.2, random_state=rando)
+
+Xii_train, Xii_test, yii_train, yii_test = train_test_split(
+    X_II, y_II, test_size=0.2, random_state=rando)
+
+Xiii_train, Xiii_test, yiii_train, yiii_test = train_test_split(
+    X_III, y_III, test_size=0.2, random_state=rando)
+
+Xiv_train, Xiv_test, yiv_train, yiv_test = train_test_split(
+    X_IV, y_IV, test_size=0.2, random_state=rando)
+
+#%%% Save Demographic Info on overall data
+es_xttest = Xt_test[['Ethnic Groupa']].copy()
+cs_xttest = Xt_test[['Community groupc ']].copy()
+
+es_xttrain = Xt_train[['Ethnic Groupa']].copy()
+cs_xttrain = Xt_train[['Community groupc ']].copy()
+
+# %%% Drop Ethinic group and commmunity group data
+
+Xt_test = Xt_test.drop(labels=['Ethnic Groupa', 'Community groupc '], axis=1)
+Xt_train = Xt_train.drop(labels=['Ethnic Groupa', 'Community groupc '], axis=1)
+
+Xi_test = Xi_test.drop(labels=['Ethnic Groupa', 'Community groupc '], axis=1)
+Xi_train = Xi_train.drop(labels=['Ethnic Groupa', 'Community groupc '], axis=1)
+
+Xii_test = Xii_test.drop(labels=['Ethnic Groupa', 'Community groupc '], axis=1)
+Xii_train = Xii_train.drop(labels=['Ethnic Groupa', 'Community groupc '], axis=1)
+
+Xiii_test = Xiii_test.drop(labels=['Ethnic Groupa', 'Community groupc '], axis=1)
+Xiii_train = Xiii_train.drop(labels=['Ethnic Groupa', 'Community groupc '], axis=1)
+
+Xiv_test = Xiv_test.drop(labels=['Ethnic Groupa', 'Community groupc '], axis=1)
+Xiv_train = Xiv_train.drop(labels=['Ethnic Groupa', 'Community groupc '], axis=1)
+
+# %%% Normalization
+
+# Normalize pH
+Xt_train['pH'] = Xt_train['pH']/14
+Xt_test['pH'] = Xt_test['pH']/14
+
+Xi_train['pH'] = Xi_train['pH']/14
+Xi_test['pH'] = Xi_test['pH']/14
+
+Xii_train['pH'] = Xii_train['pH']/14
+Xii_test['pH'] = Xii_test['pH']/14
+
+Xiii_train['pH'] = Xiii_train['pH']/14
+Xiii_test['pH'] = Xiii_test['pH']/14
+
+Xiv_train['pH'] = Xiv_train['pH']/14
+Xiv_test['pH'] = Xiv_test['pH']/14
+
+# Normalize 16s RNA data
+Xt_train.iloc[:, 1::] = Xt_train.iloc[:, 1::]/100
+Xt_test.iloc[:, 1::] = Xt_test.iloc[:, 1::]/100
+
+Xi_train.iloc[:, 1::] = Xi_train.iloc[:, 1::]/100
+Xi_test.iloc[:, 1::] = Xi_test.iloc[:, 1::]/100
+
+Xii_train.iloc[:, 1::] = Xii_train.iloc[:, 1::]/100
+Xii_test.iloc[:, 1::] = Xii_test.iloc[:, 1::]/100
+
+Xiii_train.iloc[:, 1::] = Xiii_train.iloc[:, 1::]/100
+Xiii_test.iloc[:, 1::] = Xiii_test.iloc[:, 1::]/100
+
+Xiv_train.iloc[:, 1::] = Xiv_train.iloc[:, 1::]/100
+Xiv_test.iloc[:, 1::] = Xiv_test.iloc[:, 1::]/100
+
+# %%% Binary y
+yt_train[yt_train < 7] = 0
+yt_train[yt_train >= 7] = 1
+
+yt_test[yt_test < 7] = 0
+yt_test[yt_test >= 7] = 1
+
+yi_train[yi_train < 7] = 0
+yi_train[yi_train >= 7] = 1
+
+yi_test[yi_test < 7] = 0
+yi_test[yi_test >= 7] = 1
+
+yii_train[yii_train < 7] = 0
+yii_train[yii_train >= 7] = 1
+
+yii_test[yii_test < 7] = 0
+yii_test[yii_test >= 7] = 1
+
+yiii_train[yiii_train < 7] = 0
+yiii_train[yiii_train >= 7] = 1
+
+yiii_test[yiii_test < 7] = 0
+yiii_test[yiii_test >= 7] = 1
+
+yiv_train[yiv_train < 7] = 0
+yiv_train[yiv_train >= 7] = 1
+
+yiv_test[yiv_test < 7] = 0
+yiv_test[yiv_test >= 7] = 1
+# %% Logistic Regression (Comm Isolated)
+#%%% I
+clflri = ethnic_train_metric_pipe("Logistic Regression", Xi_train, yi_train, "Blues")
+
+#%%% II
+clflrii = ethnic_train_metric_pipe("Logistic Regression", Xii_train, yii_train, "Reds")
+
+#%%% III
+clflriii = ethnic_train_metric_pipe("Logistic Regression", Xiii_train, yiii_train, "Greens")
+
+#%%% IV
+clflriv = ethnic_train_metric_pipe("Logistic Regression", Xiv_train, yiv_train, "Purples")
+
+#%%% Stack
+clflrt = ethnic_stack_pipe(clflrh,clflrb,clflra,clflrh, "Oranges")
+
+#%% Random Forest (Comm Isolated)
+#%%% I
+clfrfw = ethnic_train_metric_pipe("Random Forest", Xi_train, yi_train, "Blues")
+
+#%%% II
+clfrfb = ethnic_train_metric_pipe("Random Forest", Xii_train, yii_train, "Reds")
+
+#%%% III
+clfrfa = ethnic_train_metric_pipe("Random Forest", Xiii_train, yiii_train, "Greens")
+
+#%%% IV
+clfrfh = ethnic_train_metric_pipe("Random Forest", Xiv_train, yiv_train, "Purples")
+#%%% Stack
+clfrft = ethnic_stack_pipe(clfrfw,clfrfb,clfrfa,clfrfh, "Oranges")
+
+#%% Multinomial Naive Bayes (Comm Isolated)
+#%%% I
+clfmnbw = ethnic_train_metric_pipe("MNB", Xi_train, yi_train, "Blues")
+
+#%%% II
+clfmnbb = ethnic_train_metric_pipe("MNB", Xii_train, yii_train, "Reds")
+
+#%%% III
+clfmnba = ethnic_train_metric_pipe("MNB", Xiii_train, yiii_train, "Greens")
+
+#%%% IV
+clfmnbh = ethnic_train_metric_pipe("MNB", Xiv_train, yiv_train, "Purples")
 #%%% Stack
 clfmnbt = ethnic_stack_pipe(clfmnbw,clfmnbb,clfmnba,clfmnbh, "Oranges")
