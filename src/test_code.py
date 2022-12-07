8f20cf002d6cdada38fb3b3384dc84754682290f
ramenfeast <116684033+ramenfeast@users.noreply.github.com>
2022-12-07 12:02:57 -0500

    updated to test all current src code

--- src/test_code.py
+++ src/test_code.py
@@ -7,17 +7,19 @@ Created on Thu Dec  1 10:05:21 2022
 import time
 start_time = time.time()
 
+import pandas as pd
 from utils import get_XY, ethnicities
-from group_split import ethnic_split
+from group_split import ethnic_split, ethnic_test_train_spread
 from model_training import class_train, ethnic_stack_train
-from group_metrics import standard_metrics, ethnic_metrics, ethnic_accuracy_grid, ethnic_pred_metric_pipe, ethnic_acc_breakdown
+from group_metrics import ethnic_pred_metric_pipe, ethnic_acc_breakdown
 
-random = 2022
+random = 20
 #%%
 
 
 X,y = get_XY()
 ethnic_index = ethnicities()
+training_index = pd.DataFrame(ethnic_index)
 
 (Xt_train, Xt_test, yt_train, yt_test, 
  Xw_train, Xw_test, yw_train, yw_test,
@@ -25,6 +27,8 @@ ethnic_index = ethnicities()
  Xa_train, Xa_test, ya_train, ya_test,
  Xh_train, Xh_test, yh_train, yh_test) = ethnic_split(X,y,ethnic_index, random_state = random)
 
+train_spread, test_spread, train_label_dist, test_label_dist = ethnic_test_train_spread(X,y,ethnic_index, random)
+#%%
 clfw = class_train("Random Forest", Xw_train, yw_train, random_state = random)
 clfb = class_train("Random Forest", Xb_train, yb_train, random_state = random)
 clfa = class_train("Random Forest", Xa_train, ya_train, random_state = random)
